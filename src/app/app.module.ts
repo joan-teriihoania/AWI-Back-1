4d34760fa448d1056c6f457f80057e6c39a44892
Alexandre-Fernique <alexandre.fernique@etu.umontpellier.fr>
2021-11-09 00:29:51 +0100

    Create Angular Project AWI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {RouterModule} from "@angular/router";
 
 import { AppComponent } from './app.component';
+import { ERR404Component } from './Page/Error/err404/err404.component';
+import { RootComponent } from "./root/root.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ERR404Component,
+    RootComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path:"",component:AppComponent},
+      {path:'**',component:ERR404Component}
+    ])
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [RootComponent]
 })
 export class AppModule { }
