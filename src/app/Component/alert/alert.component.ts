c053d5d05a906116c52e99d55403bb33933e1185
Alexandre FERNIQUE <alexandre.fernique@etu.umontpellier.fr>
2021-11-25 19:23:29 +0100

    Modification du composant Alert

--- src/app/Component/alert/alert.component.ts
+++ src/app/Component/alert/alert.component.ts
@@ -1,43 +1,22 @@
 import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 
 
-import {animate, state, style, transition, trigger} from "@angular/animations";
 
 
 @Component({
   selector: 'app-alert',
   templateUrl: './alert.component.html',
-  styleUrls: ['./alert.component.css'],
-  animations: [
-    trigger('animation', [
-      // ...
-      state('show', style({
-        opacity: 1,
-      })),
-      state('hidden', style({
-        opacity: 0,
-      })),
-      transition('* => *', [
-        animate('1s')
-      ]),
-    ]),
-  ],
+  styleUrls: ['./alert.component.css']
+
 })
 
-export class AlertComponent implements OnInit,OnChanges {
+export class AlertComponent implements OnInit {
   @Input() etat:string|undefined;
-  @Input() active:boolean|undefined;
   @Input() text:string|undefined;
-  success:string;
-  danger:string;
+
   constructor() {
-    this.success="show";
-    this.danger="hidden";
     if (this.etat==undefined){
       this.etat="";
-    }
-    if (this.active==undefined){
-      this.active=false;
     }
     if (this.text==undefined){
       this.text="";
@@ -48,16 +27,7 @@ export class AlertComponent implements OnInit,OnChanges {
   ngOnInit(): void {
 
   }
-  ngOnChanges(changes: SimpleChanges) {
-    this.change()
-  }
-  change(){
-    if(this.etat=="sucess"){
-      this.success="show";
-    }else if(this.etat=="danger"){
-      this.danger="show";
-    }
-  }
+
 
 
 }
--- src/app/Page/ingredient/ingredient.component.ts
+++ src/app/Page/ingredient/ingredient.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {IngredientService} from "../../Service/ingredient.service";
 import {Ingredient} from "../../class/ingredient";
 import {Category} from "../../class/category";
+import {animate, state, style, transition, trigger} from "@angular/animations";
+import {timeout} from "rxjs";
 
 
 
@@ -9,15 +11,36 @@ import {Category} from "../../class/category";
 @Component({
   selector: 'app-ingredient',
   templateUrl: './ingredient.component.html',
-  styleUrls: ['./ingredient.component.css']
+  styleUrls: ['./ingredient.component.css'],
+  animations: [
+    trigger('animation', [
+      // ...
+      state('show', style({
+        opacity: 1,
+      })),
+      state('hidden', style({
+        opacity: 0,
+      })),
+      transition('show => hidden', [
+        animate('1s')
+      ]),
+    ]),
+  ],
 })
 export class IngredientComponent implements OnInit {
   category: Array<Category>;
   ingredient:Array<Ingredient>
+  active:boolean;
+  text:string;
+  etat:string;
+
 
   constructor(private request:IngredientService) {
     this.category=request.getIcategory();
     this.ingredient=request.getAllIngredient();
+    this.active=false;
+    this.text="";
+    this.etat="";
 
 
   }
@@ -27,6 +50,20 @@ export class IngredientComponent implements OnInit {
   }
   add(event:Ingredient){
     this.ingredient.push(event);
+  }
+  errorAlert(){
+    this.text="ça marche pas"
+    this.etat="danger"
+    this.active=true;
+    setTimeout(() => this.active=false, 1000);
+
+  }
+  successAlert(){
+    this.text="ça marche"
+    this.etat="success"
+    this.active=true;
+    setTimeout(() => this.active=false, 1000);
+
   }
   delete(id:number|undefined){
     if(id==undefined){
