4441b085cc083a35bc41cbd5986da9a1751edcfa
Alexandre <alexandre.fernique@etu.umontpellier.fr>
2022-01-05 17:36:27 +0100

    Refactoring project New Repositery for Awi-Back

--- src/app/class/stepable.ts
+++
@@ -1,9 +0,0 @@
-import {Step} from "./step";
-
-export interface Stepable {
-  id:number;
-  getStep():Array<Step>;
-  getType():string;
-  getCoutIngredient(nbTable:number):number
-  getTotalDuration():number
-}
